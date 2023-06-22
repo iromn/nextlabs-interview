dcd96fda9d7ea62a3e231e883f4d1c49cc4fdc41
Nganba-Irom <65939683+iromn@users.noreply.github.com>
2023-06-22 22:40:48 +0530

    Build commit

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'sweetalert2/dist/sweetalert2.min.css';
-
-createApp(App).use(router,VueSweetalert2).mount('#app')
